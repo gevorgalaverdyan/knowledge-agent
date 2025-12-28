8b2c731477b7449990cdab869eff1e3ef67fe462
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2025-12-28 16:15:47 -0500

    display chats + messages

--- apps/frontend/src/app/shared/components/message.component/message.component.ts
+++ apps/frontend/src/app/shared/components/message.component/message.component.ts
@@ -1,24 +1,38 @@
-import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { Component, inject, Input, OnInit, OnChanges, SimpleChanges, signal } from '@angular/core';
 import { ZardCardComponent } from '../card/card.component';
 import { MarkdownComponent } from 'ngx-markdown';
 import { ZardSkeletonComponent } from '../skeleton/skeleton.component';
 import { MessageService } from '@/shared/services/message.service';
 import { Message } from '@/models/chat';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-message',
-  imports: [ZardCardComponent, ZardSkeletonComponent, MarkdownComponent],
+  imports: [ZardCardComponent, ZardSkeletonComponent, MarkdownComponent, DatePipe],
   templateUrl: './message.component.html',
 })
-export class MessageComponent implements OnInit {
+export class MessageComponent implements OnInit, OnChanges {
   @Input() chat_id: string = '';
   messageService = inject(MessageService);
-  loading: boolean = false;
+  loading = signal(false);
   messages = signal<Message[]>([]);
 
   ngOnInit() {
+    this.fetchMessages();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['chat_id'] && !changes['chat_id'].firstChange) {
+      this.fetchMessages();
+    }
+  }
+
+  fetchMessages() {
+    if (!this.chat_id) return;
+    this.loading.set(true);
     this.messageService.getMessages(this.chat_id).subscribe((data: any) => {
       this.messages.set(data.messages);
+      this.loading.set(false);
     });
   }
 }
